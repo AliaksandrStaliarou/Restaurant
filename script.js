0d774d5887132fbe4f0e9ebf7b41788f5b077942
AliaksandrStaliarou <staliarou@gmail.com>
2018-05-22 16:31:42 +0300

    commit

--- script.js
+++ script.js
@@ -2,18 +2,27 @@
  * Created by נ4םנ on 19.05.2018.
  */
 
+/*hamburger animation and menu opener*/
 var hamburgerButton = document.querySelector('.hamburger');
-var navSocial = document.querySelector('.navSocial');
 hamburgerButton.addEventListener('click', openHambugerButton);
 
 function openHambugerButton() {
     hamburgerButton.classList.toggle('open_hamburger');
+    var navSocial = document.querySelector('.navSocial');
     navSocial.classList.toggle('open_navSocial');
-    /*if (navSocial.style.maxHeight){
-        navSocial.style.maxHeight = null;
-    } else {
-        navSocial.style.maxHeight = 285 + "px";
-    }*/
-
 }
+
+/*sticky menu*/
+window.onscroll = function() {stickyMenu()};
+
+function stickyMenu() {
+    var wrapper = document.querySelector('.wrapper');
+    var sticky = wrapper.offsetTop;
+    if (window.pageYOffset > sticky && document.documentElement.clientWidth >= 990) {
+        wrapper.classList.add("sticky")
+    } else {
+        wrapper.classList.remove("sticky");
+    }
+}
+
 
